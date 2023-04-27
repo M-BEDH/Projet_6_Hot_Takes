6ac6000ea7e2938989114e6d4372ea63dbd9db53
M-BEDH <mel.b.ms@pm.me>
2023-04-27 10:44:02 +0200

    retrait console.log

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -8,7 +8,6 @@ module.exports = (req, res, next) => {
        req.auth = {
            userId
        };
-     //  console.log('token :' + token)
      next();
    } catch(error) {
        res.status(401).json({ error });
